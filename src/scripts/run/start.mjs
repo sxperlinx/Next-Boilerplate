65372450c5b886017498e1b3c84e3eaa991bf598
sxpersxnic <102521739+sxperlinx@users.noreply.github.com>
2025-05-20 10:31:10 +0000

    Removed scripts since they are redundant

--- src/scripts/run/start.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { run } from '../util.mjs';
-
-const tasks = [
-	{ msg: 'Building the project', cmd: 'next build' },
-	{ msg: 'Running the project', cmd: 'next start' },
-];
-
-run(tasks);
